40e434d0848d517671a409c260db55b412dcf8b3
Hack Reactor Students <jonathan.reem@gmail.com>
2014-04-18 19:24:45 -0700

    Added bTree tests.

--- sprint-two/spec/bTreeSpec.js
+++ sprint-two/spec/bTreeSpec.js
@@ -3,12 +3,41 @@ var assert = chai.assert;
 
 describe("b-tree", function () {
   var bTree;
+  var stuff = [];
+  var min = 0;
+  var max = 100;
+  var size = 100;
+  for (var i = 0; i < size; i++) {
+    stuff.push(Math.floor(Math.random()*(max - min + 1) + min));
+  }
 
   beforeEach(function () {
     bTree = new BTree();
   });
 
-  it("should have methods: ", function () {
-    expect(bTree.method).to.be.a('function');
+  it("should have methods: insert, contains, find", function () {
+    expect(bTree.insert).to.be.a('function');
+    expect(bTree.contains).to.be.a('function');
+    expect(bTree.find).to.be.a('function');
   });
+
+  it("should insert and contain one value", function(){
+    bTree.insert(7);
+    expect(bTree.contains(7));
+  });
+
+  it("should insert and contain several values", function(){
+    _.each(stuff, function(val){
+      bTree.insert(val);
+    });
+
+    var result = true;
+    _.each(stuff, function(val){
+      result = result && bTree.contains(val);
+    });
+
+    expect(result);
+  });
+
+
 });
