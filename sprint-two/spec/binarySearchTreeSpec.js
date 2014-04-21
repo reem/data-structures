3a620384f77a2a38e9ef83117ddf040d6741da33
Jonathan Reem <jonathan.reem@gmail.com>
2014-04-21 14:01:16 -0700

    Enchanced binary search tree.

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -10,7 +10,7 @@ describe("binarySearchTree", function() {
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
     expect(binarySearchTree.insert).to.be.a('function');
     expect(binarySearchTree.contains).to.be.a('function');
-    expect(binarySearchTree.depthFirstLog).to.be.a('function');
+    expect(binarySearchTree.each).to.be.a('function');
   });
 
   it("should insert values at the correct location in the tree", function(){
@@ -32,10 +32,10 @@ describe("binarySearchTree", function() {
 
   it("should execute a callback on every value in a tree using 'depthFirstLog'", function(){
     var array = [];
-    var func = function(value){ array.push(value); }
+    var func = function(value){ array.push(value); };
     binarySearchTree = binarySearchTree.insert(2);
     binarySearchTree = binarySearchTree.insert(3);
-    binarySearchTree.depthFirstLog(func);
+    binarySearchTree.each(func);
     assert.notStrictEqual(array, [5,2,3]);
   });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,25 +1,36 @@
 var makeBinarySearchTree = function(value, left, right) {
+  // Persistent Binary Search Tree data structure.
+  // Uses the Functional instantiation style.
+
+  // Provides an immutable interface for insertion that
+  // supports memory sharing.
   return extend({
-    left : left || null,
+    left : left || null, // null represents a "leaf" node.
     right : right || null,
     value: value
   }, binaryTreeMethods);
 };
 
+// Will contain the "prototype" of this data-structure.
 var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function(value) {
   if (this.value < value) {
-    if(this.right === null){
-      return makeBinarySearchTree(this.value, this.left, makeBinarySearchTree(value));
+    // must check here or we might try to access a property on null 
+    if(this.right === null) {
+      return makeBinarySearchTree(this.value, this.left, 
+        makeBinarySearchTree(value));
     }
-    return makeBinarySearchTree(this.value, this.left, this.right.insert(value));
+    return makeBinarySearchTree(this.value, this.left, 
+      this.right.insert(value));
   }
   if(this.value > value) {
     if(this.left === null) {
-      return makeBinarySearchTree(this.value, makeBinarySearchTree(value), this.right);
+      return makeBinarySearchTree(this.value, 
+        makeBinarySearchTree(value), this.right);
     }
-    return makeBinarySearchTree(this.value, this.left.insert(value), this.right);
+    return makeBinarySearchTree(this.value, 
+      this.left.insert(value), this.right);
   }
   if (this.value === value) {
     return this;
@@ -44,13 +55,16 @@ binaryTreeMethods.contains = function(value){
   }
 };
 
-binaryTreeMethods.depthFirstLog = function (callback) {
+binaryTreeMethods.each = function (callback) {
   callback(this.value);
-  this.left  && this.left.depthFirstLog(callback);
-  this.right && this.right.depthFirstLog(callback);
+  // Check that left and right are not null before
+  // we try and recurse on them.
+  this.left  && this.left.each(callback);
+  this.right && this.right.each(callback);
 };
 
 var extend = function(to, from){
+  // Pseudo-copy of _.extend, returns to.
   for(var key in from){
     to[key] = from[key];
   }
